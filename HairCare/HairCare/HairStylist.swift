12852894a79ec67b07289b07b95d4536750f709a
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-08-26 10:27:44 -0700

    Added 5 Models

---
+++ HairCare/HairCare/HairStylist.swift
@@ -0,0 +1,14 @@
+//
+//  HairStylist.swift
+//  HairCare
+//
+//  Created by Taylor Lyles on 8/26/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import Foundation
+
+struct HairStylist: Codable {
+	var name: String
+	var yearsOfExperience: Int
+}
